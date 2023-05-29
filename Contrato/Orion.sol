38ccd7bc852c53ec458ede73b991258ad4238e08
@DevPaulo28 <102810087+DevPaulo70008@users.noreply.github.com>
2023-05-29 19:01:43 -0300

    Orion.sol

--- Contrato/Orion.sol
+++ Contrato/Orion.sol
@@ -1,1 +1,58 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "./@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "./@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "./@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+import "./@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
+import "./@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "./@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "./@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract Orion is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __ERC20_init("orion", "ORN");
+        __ERC20Burnable_init();
+        __ERC20Permit_init("orion");
+        __ERC20Votes_init();
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+
+        _mint(msg.sender, 10000000 * 10 ** decimals());
+    }
+
+    function _authorizeUpgrade(address newImplementation)
+        internal
+        onlyOwner
+        override
+    {}
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._burn(account, amount);
+    }
+}
